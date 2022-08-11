193707fd40e69d350670296f28226ec3094873fc
Pawel Wojtarowicz <wojtarowicz.pawel@gmail.com>
2022-08-11 12:05:53 +0200

    added decrypting passwords and printing to txt area

--- app.py
+++ app.py
@@ -8,7 +8,7 @@ from cryptography.fernet import Fernet
 root = Tk()
 root.title("Password Manager")
 root.eval('tk::PlaceWindow . center')
-root.geometry("300x420")
+root.geometry("400x420")
 
 
 def create_secret_key():
@@ -26,13 +26,23 @@ def load_secret_key():
 def save_to_file(site, login, password):
     with open("passwords.txt", "a+") as f:
         encrypted = Fernet(load_secret_key()).encrypt(password.encode())
-        f.write(site + ":" + login + "|" + encrypted.decode() + "\n")
+        f.write(site + ":" + login + ":" + encrypted.decode() + "\n")
 
 
-def read_password_file():
-    # TODO:
-    pass
+def load_password_file():
+    passwords = []
+    index = 0
+    with open("passwords.txt", "r") as f:
+        for line in f:
+            index +=1
+            site, login, encrypted = line.split(":")  
+            decrypted = Fernet(load_secret_key()).decrypt(encrypted.encode())
+            password = decrypted.decode('utf-8')
+            passwords.append(str(index) + ". " + site + ": " + login + ": " + password)
+    for password in passwords:
+        txt_area.insert(END, password + "\n")
 
+        
 
 def generate_password():
     password_input.delete(0, END)
@@ -55,17 +65,17 @@ password_label = Label(text="Password", pady=5)
 password_label.grid(column=0, row=2, sticky=E)
 
 # inputs
-website_input = Entry(width=35)
+website_input = Entry(width=55)
 website_input.grid(column=1, row=0)
-login_input = Entry(width=35)
+login_input = Entry(width=55)
 login_input.grid(column=1, row=1)
 login_input.insert(0, "name@email.com")
-password_input = Entry(width=35)
+password_input = Entry(width=55)
 password_input.grid(column=1, row=2)
 password_input.insert(0, "************")
 
 # buttons
-read_button = Button(text="Open file", command=read_password_file)
+read_button = Button(text="Open file", command=load_password_file)
 read_button.grid(column=1, row=3, sticky="WE", pady=5)
 generate_button = Button(text="Generate Password", command=generate_password)
 generate_button.grid(column=1, row=4, sticky="WE", pady=5)
@@ -75,7 +85,7 @@ exit_button = Button(text="Exit", command=exit)
 exit_button.grid(column=1, row=7, sticky="WE", pady=10)
 
 # text area
-txt_area = Text(root, width=25, height=10, pady=5)
+txt_area = Text(root, width=40, height=10, pady=5)
 txt_area.grid(column=1, row=6)
 
 
