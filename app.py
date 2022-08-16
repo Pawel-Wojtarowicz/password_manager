33b0448f0ac69e7c8ded2faa4b4411a0438d8b97
Pawel Wojtarowicz <wojtarowicz.pawel@gmail.com>
2022-08-16 11:46:23 +0200

    changed saved to file method and added showerror

--- app.py
+++ app.py
@@ -2,7 +2,9 @@ from tkinter import *
 import string
 import random
 from os import path
+from tkinter import messagebox
 from cryptography.fernet import Fernet
+
 
 # init
 root = Tk()
@@ -23,26 +25,32 @@ def load_secret_key():
         return key
 
 
-def save_to_file(site, login, password):
-    with open("passwords.txt", "a+") as f:
-        encrypted = Fernet(load_secret_key()).encrypt(password.encode())
-        f.write(site + ":" + login + ":" + encrypted.decode() + "\n")
+def save_to_file():
+    website = website_input.get()
+    login = login_input.get()
+    password = password_input.get()
+
+    if len(website) == 0 or len(login) == 0 or len(password) == 0:
+        messagebox.showerror(
+            title="Error!", message="Don't leave any empty boxes!")
+    else:
+        with open("passwords.txt", "a+") as f:
+            encrypted = Fernet(load_secret_key()).encrypt(password.encode())
+            f.write(website + ":" + login + ":" + encrypted.decode() + "\n")
 
 
 def load_password_file():
+    txt_area.delete("1.0", END)
     passwords = []
-    index = 0
     with open("passwords.txt", "r") as f:
         for line in f:
-            index +=1
-            site, login, encrypted = line.split(":")  
+            site, login, encrypted = line.split(":")
             decrypted = Fernet(load_secret_key()).decrypt(encrypted.encode())
             password = decrypted.decode('utf-8')
-            passwords.append(str(index) + ". " + site + ": " + login + ": " + password)
+            passwords.append(site + ": " + login + ": " + password)
     for password in passwords:
         txt_area.insert(END, password + "\n")
 
-        
 
 def generate_password():
     password_input.delete(0, END)
@@ -72,14 +80,14 @@ login_input.grid(column=1, row=1)
 login_input.insert(0, "name@email.com")
 password_input = Entry(width=55)
 password_input.grid(column=1, row=2)
-password_input.insert(0, "************")
+#password_input.insert(0, "************")
 
 # buttons
 read_button = Button(text="Open file", command=load_password_file)
 read_button.grid(column=1, row=3, sticky="WE", pady=5)
 generate_button = Button(text="Generate Password", command=generate_password)
 generate_button.grid(column=1, row=4, sticky="WE", pady=5)
-store_to_file_button = Button(text="Store to file")
+store_to_file_button = Button(text="Store to file", command=save_to_file)
 store_to_file_button.grid(column=1, row=5, sticky="WE", pady=5)
 exit_button = Button(text="Exit", command=exit)
 exit_button.grid(column=1, row=7, sticky="WE", pady=10)
